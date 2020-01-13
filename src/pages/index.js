763bbedbe6a21f62902a953985afc648c3dd07e3
Lucas Hiago <lucashiago63@gmail.com>
2020-01-12 23:46:39 -0300

    Modify index and create about page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,9 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <div style={{ color: `purple` }}>
+    <h1>Hello Gatsby!</h1>
+    <p>Hot reloading is cool</p>
+    <img src="https://source.unsplash.com/random/400x200" alt="Random image"></img>
+  </div>
+)
