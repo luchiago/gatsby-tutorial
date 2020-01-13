5095c7d7d9f4c5b64b85b1598159feacee52d9cf
Lucas Hiago <lucashiago63@gmail.com>
2020-01-12 23:58:29 -0300

    Finish first section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,11 @@
 import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
 
 export default () => (
   <div style={{ color: `purple` }}>
-    <h1>Hello Gatsby!</h1>
+    <Link to="/contact/"> Contact</Link> 
+    <Header headerText="Hello Gatsby!" />
     <p>Hot reloading is cool</p>
     <img src="https://source.unsplash.com/random/400x200" alt="Random image"></img>
   </div>
